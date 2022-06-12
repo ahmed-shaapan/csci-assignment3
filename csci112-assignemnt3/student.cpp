1a4de9d4f2622cd456a4e7720190c7eb3a518fdd
MohamedQushta <99907912+MohamedQushta@users.noreply.github.com>
2022-06-12 23:13:07 +0200

    Update student.cpp
    Added the function (register students)
    Finished all setters and getters
    Added the function (getstudentinfo) without score and grades

--- csci112-assignemnt3/student.cpp
+++ csci112-assignemnt3/student.cpp
@@ -10,38 +10,123 @@ private:
 	char student_name[50];
 	int student_ID;
 	char student_email_username[10];
-	char student_major[10];
-	char student_grades[5];
+	char student_major[50];
+	float student_grades[5];
 	float student_score;
 	char student_email_password[10];
 
 public:
-	// Setters
-	void set_student_name(char* value) {
-		strcpy(student_name, value);
+    void registerStudent()
+    {
+        char name[50];
+        int ID;
+        char username[10];
+        char major[10];
+        float grades[5];
+        float score;
+        char password[10];
+
+        cout << "Enter Student Name: ";
+        fgets(name,50,stdin);
+        set_student_name(name);
+
+        cout << "Enter Student ID: ";
+        scanf("%d", &ID);
+        set_student_id(ID);
+        fflush(stdin);
+
+        cout << "Enter Student Major: ";
+        fgets(major,50,stdin);
+        set_student_major(major);
+        fflush(stdin);
+
+        cout << "Enter Student Username: ";
+        fgets(username,10,stdin);
+        set_student_email_username(username);
+        fflush(stdin);
+
+        cout << "Enter Student Password: ";
+        gets(password);
+        set_student_email_password(password);
+    /*
+        cout << "Enter Student Grades (5 courses): ";
+        for(int i = 5; i<5;++i)
+            cin >>grades[i];
+        set_student_grades(grades);
+    */
+
+    }
+
+	//Name
+	void set_student_name(char* name) {
+		strcpy(student_name, name);
 	}
+	char* get_student_name()
+	{
+	    return student_name;
+	}
+	//ID
 	void set_student_id(int ID) {
 		student_ID = ID;
 	}
-	void set_student_email_username(char* value) {
-		strcpy(student_email_username, value);
-
+	int get_student_ID()
+	{
+	    return student_ID;
 	}
-	void set_student_major(char* value) {
-		strcpy(student_major, value);
+	//Username
+	void set_student_email_username(char* username) {
+		strcpy(student_email_username, username);
 	}
-	void set_student_grades(char* value) {
-		strcpy(student_grades, value);
+	char* get_student_username()
+	{
+	    return student_email_username;
 	}
+	//Password
+	void set_student_email_password(char* password)
+	{
+	    strcpy(student_email_password, password);
+	}
+	char* get_student_password()
+	{
+	    return student_email_password;
+	}
+	//Major
+	void set_student_major(char* major) {
+		strcpy(student_major, major);
+	}
+	char* get_student_major()
+	{
+	    return student_major;
+	}
+	//Grades
+	void set_student_grades(float gds[5]) {
+		for(int i = 0; i<5;++i)
+            student_grades[i] = gds[i];
+	}
+	//Score
 	void set_student_score(float value) {
-		student_score = value;
+	    int sum;
+		for(int i = 0; i<5;++i)
+            sum += student_grades[i];
+        student_score = sum/100;
 	}
-	void set_student_email_password(char* value) {
-		strcpy(student_email_password, value);
+	float get_student_score()
+	{
+	    return student_score;
 	}
+
 	//Getters
-	void get_student_name() {
-		cout << student_name << "\n" << "hello baby";
+	void get_student_info() {
+		cout << "Name: " << get_student_name();
+		cout << "ID: " << get_student_ID() << "\n";
+        cout << "Major: " << get_student_major();
+		cout << "Email Username: " << get_student_username();
+		cout << "Email Password: " << get_student_password();
+		/*
+		cout << "Grades: " << student_grades << endl;
+
+		cout << "Score: " << student_score << endl;
+		*/
 	}
 
 
@@ -50,6 +135,7 @@ public:
 int main()
 {
 	CStudent obj1;
-	obj1.set_student_name("mohamed");
-	obj1.get_student_name();
+	CStudent * objp=&obj1;
+    obj1.registerStudent();
+	obj1.get_student_info();
 }
