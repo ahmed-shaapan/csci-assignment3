45fdc71bf5f5356e41ae64c5a1969cea26f77c06
Mohamed ElGemeie <mgalal2002@outlook.com>
2022-06-16 10:59:31 +0200

    Update main.cpp

--- csci112-assignemnt3/main.cpp
+++ csci112-assignemnt3/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "student.h"
+#include <cstring>
+#include<string>
+
 using namespace std;
+
 int main()
 {
-    cout << "Hello World!\n";
+	CStudent obj1;
+	CStudent* objp = &obj1;
+	obj1.registerStudent();
+	obj1.get_student_info();
 }
+
